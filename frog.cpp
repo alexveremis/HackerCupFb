daf3961b593cb751df47d0381b52c3d4b3c2cf10
alexveremis <34975379+alexveremis@users.noreply.github.com>
2019-06-15 16:52:13 +0300

    Add files via upload

---
+++ frog.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+int i=0,j=0,argrammwn;
+char c;
+ifstream fp;
+fp.open(argv[1]);
+fp >>argrammwn;
+fp.get(c);
+//cout<<argrammwn<<c;
+char apantiseis[argrammwn];
+bool flag=false;
+ while (fp.get(c)) {
+	if (c=='.') {
+		flag=true;
+		j--;}
+        else if (c=='\n') {
+		if (j-1>=0 && flag==true)apantiseis[i]='Y';
+		else apantiseis[i]='N';
+		i++;
+		flag =false;
+		j=0;
+			}
+	else {
+		j++;}
+    }
+
+fp.close();
+
+ofstream myfile;
+myfile.open ("output.txt");
+ for(i = 0; i< argrammwn; i++){
+    myfile<<"Case #"<<i+1<<": " << apantiseis[i]<<'\n';
+    }
+ myfile.close();
+ return 0;
+}
